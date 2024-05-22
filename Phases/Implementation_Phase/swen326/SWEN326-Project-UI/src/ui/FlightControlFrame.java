91cb543710ce490e82af3b2e93d6d6c73b39ac6a
perirish <perirish@myvuw.ac.nz>
2024-05-22 14:34:35 +1200

    Added autopilot light and text fields for input

--- Phases/Implementation_Phase/swen326/SWEN326-Project-UI/src/ui/FlightControlFrame.java
+++ Phases/Implementation_Phase/swen326/SWEN326-Project-UI/src/ui/FlightControlFrame.java
@@ -1,26 +1,44 @@
 package ui;
 
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class FlightControlFrame extends JFrame {
+public class FlightControlFrame extends JFrame implements ActionListener{
 
-    private JPanel mapPanel;
+	private static final long serialVersionUID = 1L;
+	private JPanel mapPanel;
     private JPanel flightManagementPanel;
     private JPanel autopilotControlPanel;
     private JPanel sensorDataDisplayPanel;
     private JPanel hazardAlertsPanel;
     private int height = 600;
-    private int width = 1400;
-    ImageIcon flightIcon;
-    ImageIcon map;
-    Border border;
+    private int width = 1200;
+    private JLabel autopilotLight;
+    private ImageIcon flightIcon;
+    private ImageIcon map;
+    private ImageIcon autopilotOn;
+    private ImageIcon autopilotOff;
+    private ImageIcon autopilotFaulty;
+    private Border border;
+    private JCheckBox autopilotEngagedButton;
 
     public FlightControlFrame() {
 
-        this.flightIcon = new ImageIcon("Images/flight.png");
+        this.flightIcon = new ImageIcon("src/ui/Images/flight.png");
+        this.map = new ImageIcon(new ImageIcon("src/ui/Images/world_map.jpg").getImage().getScaledInstance((int) ((8.0 / 14.0) * width), (int) ((3.0 / 4.0) * height - 35), Image.SCALE_DEFAULT));
+        this.autopilotOff = new ImageIcon("src/ui/Images/off_light.png");
+        this.autopilotOn = new ImageIcon("src/ui/Images/green_light.png");
+        this.autopilotFaulty = new ImageIcon("src/ui/Images/red_light.png");
+        
         this.setTitle("Flight Controller");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
@@ -37,31 +55,12 @@ public class FlightControlFrame extends JFrame {
     public void addPanels() {
         this.border = BorderFactory.createLineBorder(Color.WHITE, 3);
 
-        flightManagementPanel = new JPanel();
-        flightManagementPanel.setBounds(0, 0, (int) ((3.0 / 14.0) * width), (int) ((3.0 / 4.0) * height));
-        flightManagementPanel.setBackground(Color.YELLOW);
-        flightManagementPanel.setBorder(border);
-
-        mapPanel = new JPanel();
-        this.addMap(mapPanel);
-        mapPanel.setBounds((int) ((3.0 / 14.0) * width), 0, (int) ((8.0 / 14.0) * width), (int) ((3.0 / 4.0) * height));
-        mapPanel.setBorder(border);
-
-        autopilotControlPanel = new JPanel();
-        autopilotControlPanel.setBounds((int) ((11.0 / 14.0) * width), 0, (int) ((3.0 / 14.0) * width), (int) ((3.0 / 4.0) * height));
-        autopilotControlPanel.setBackground(Color.RED);
-        autopilotControlPanel.setBorder(border);
-
-        sensorDataDisplayPanel = new JPanel();
-        sensorDataDisplayPanel.setBounds(0, (int) ((3.0 / 4.0) * height), width / 2, height / 4);
-        sensorDataDisplayPanel.setBackground(Color.DARK_GRAY);
-        sensorDataDisplayPanel.setBorder(border);
-
-        hazardAlertsPanel = new JPanel();
-        hazardAlertsPanel.setBounds(width / 2, (int) ((3.0 / 4.0) * height), width / 2, height / 4);
-        hazardAlertsPanel.setBackground(Color.PINK);
-        hazardAlertsPanel.setBorder(border);
-
+        this.flightManagementPanel = this.getFlightManagementPanel(border);
+        this.mapPanel = this.getMapPanel(border);
+        this.autopilotControlPanel = this.getAutopilotControlPanel(border);
+        this.sensorDataDisplayPanel = this.getSensorDataDisplayPanel(border);
+        this.hazardAlertsPanel = this.getHazardAlertsPanel(border);
+        
         this.add(flightManagementPanel);
         this.add(mapPanel);
         this.add(autopilotControlPanel);
@@ -71,16 +70,115 @@ public class FlightControlFrame extends JFrame {
 
     // Add Map and Label to the center Panel
     public void addMap(JPanel panel) {
-        this.map = new ImageIcon("Images/world_map.jpg");
+    	
         JLabel label = new JLabel("Flight Route Map");
         label.setIcon(this.map);
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.BOTTOM);
+        label.setOpaque(true);
         label.setForeground(Color.WHITE);
+        label.setBackground(Color.DARK_GRAY);
         label.setBorder(border);
         label.setVerticalAlignment(JLabel.CENTER);
         label.setHorizontalAlignment(JLabel.CENTER);
         panel.add(label);
+        
     }
+    
+    
+    public JPanel getFlightManagementPanel(Border border) {
+    	
+    	// Panel
+    	flightManagementPanel = new JPanel();
+        flightManagementPanel.setBounds(0, 0, (int) ((3.0 / 14.0) * width), (int) ((3.0 / 4.0) * height));
+        flightManagementPanel.setBackground(Color.DARK_GRAY);
+        flightManagementPanel.setBorder(border);
+        
+        // Components
+        JTextField latitudeTextField = new JTextField("Enter Latitude");
+        JButton latitudeButton = new JButton("Submit Latitude");
+        
+        JTextField longitudeTextField = new JTextField("Enter Longitude");
+        JButton longitudeButton = new JButton("Submit Longitude");
+        
+        JTextField altitudeTextField = new JTextField("Enter Altitude");
+        JButton altitudeButton = new JButton("Submit Altitude");
+        
+        flightManagementPanel.add(latitudeTextField);
+        flightManagementPanel.add(latitudeButton);
+        
+        flightManagementPanel.add(longitudeTextField);
+        flightManagementPanel.add(longitudeButton);
+        
+        flightManagementPanel.add(altitudeTextField);
+        flightManagementPanel.add(altitudeButton);
+        
+        return flightManagementPanel;
+        
+    }
+    
+    public JPanel getMapPanel(Border border) {
+    	
+    	mapPanel = new JPanel();
+        this.addMap(mapPanel);
+        mapPanel.setBounds((int) ((3.0 / 14.0) * width), 0, (int) ((8.0 / 14.0) * width), (int) ((3.0 / 4.0) * height));
+        mapPanel.setBorder(border);
+        return mapPanel;
+        
+    }
+    
+    public JPanel getAutopilotControlPanel(Border border) {
+    	
+    	// Panel
+    	autopilotControlPanel = new JPanel();
+        autopilotControlPanel.setBounds((int) ((11.0 / 14.0) * width), 0, (int) ((3.0 / 14.0) * width), (int) ((3.0 / 4.0) * height));
+        autopilotControlPanel.setBackground(Color.DARK_GRAY);
+        autopilotControlPanel.setBorder(border);
+        
+        // Components
+        autopilotEngagedButton = new JCheckBox("Autopilot Engaged");
+        autopilotLight = new JLabel(this.autopilotOff);
+        
+        autopilotEngagedButton.addItemListener(e -> {
+        	if (e.getStateChange() == ItemEvent.SELECTED) {
+        		autopilotLight.setIcon(autopilotOn);
+        	} else {
+        		autopilotLight.setIcon(autopilotOff);
+        	}
+        });
+     
+        autopilotControlPanel.add(autopilotEngagedButton);
+        autopilotControlPanel.add(autopilotLight);
+        return autopilotControlPanel;
+        
+    }
+    
+    public JPanel getSensorDataDisplayPanel(Border border) {
+    	
+    	sensorDataDisplayPanel = new JPanel();
+        sensorDataDisplayPanel.setBounds(0, (int) ((3.0 / 4.0) * height), width / 2, height / 4);
+        sensorDataDisplayPanel.setBackground(Color.DARK_GRAY);
+        sensorDataDisplayPanel.setBorder(border);
+        return sensorDataDisplayPanel;
+    	
+    }
+    
+    public JPanel getHazardAlertsPanel(Border border) {
+    	
+    	hazardAlertsPanel = new JPanel();
+        hazardAlertsPanel.setBounds(width / 2, (int) ((3.0 / 4.0) * height), width / 2, height / 4);
+        hazardAlertsPanel.setBackground(Color.DARK_GRAY);
+        hazardAlertsPanel.setBorder(border);
+        return hazardAlertsPanel;
+    	
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		String s = e.getActionCommand();
+        
+		
+	}
 
 }
